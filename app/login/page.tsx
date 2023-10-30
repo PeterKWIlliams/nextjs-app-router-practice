a45fb9bdc304356b904eab088ecdb4d41a0141be
Peter Williams <peterkennethwilliams@gmail.com>
2023-10-30 07:37:38 +0000

    fixed metadata tag causing failed build

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,10 +2,6 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "../ui/login-form";
 import { Metadata } from "next";
 
-export const metaData: Metadata = {
-  title: "Login in page for the nextjs14 tutorial",
-};
-
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
